dad0698e3cd04da698005fb8add08b619b4bc963
Gal Schlezinger <gal@spitfire.co.il>
2020-02-11 14:04:01 +0200

    initial commit

---
+++ src/match-platform.ts
@@ -0,0 +1,15 @@
+import path from "path";
+import fs from "fs";
+
+function getPath() {
+  switch (process.platform) {
+    case "win32":
+      return path.join(__dirname, "../bin/mdbook-windows.exe");
+    case "darwin":
+      return path.join(__dirname, "../bin/mdbook-macos");
+    default:
+      return path.join(__dirname, "../bin/mdbook-linux");
+  }
+}
+
+fs.renameSync(getPath(), path.join(__dirname, "../bin/mdbook.exe"));
